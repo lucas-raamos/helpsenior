5fd362fe8f5a1b19f2abc7fa7ffb2799b20dae30
JordanaRTexeira <jordana.keppler@hotmail.com>
2023-09-05 21:34:38 -0300

    :art: Alterações Layout - LandigPage
    Alterações Layout - LandingPage

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,4 +1,3 @@
-
 import { Link } from 'react-router-dom';
 import logo from '../../img/logo.png'
 import styles from './LandingPage.module.css';
@@ -10,28 +9,42 @@ import vinte_quatro_hrs from '../../img/24h.png';
 import caixa from '../../img/caixa.png';
 import casa from '../../img/casa.png'
 import user_happy from '../../img/user-happy.png'
-import casa1 from '../../img/casa1.png'
-import casa2 from '../../img/casa2.png'
-import casa3 from '../../img/casa3.png'
+import alimentacao from '../../img/alimentacao.png'
+import medicamentos from '../../img/medicamentos.png'
+import rotina from '../../img/rotina.png'
+import facebook from '../../img/facebook.png'
+import twitter from '../../img/twitter.png'
+import instagram from '../../img/instagram.png'
+import linkedin from '../../img/linkedin.png'
 
 function LandingPage() {
     return (
         <div>
-            <div className={styles.background_verde}>
+            <div className={styles.background_azul}>
                 <header>
                     <div className={styles.container}>
                         <nav>
                             <div className={styles.logo_nav}>
-                            <Link to="/">
-                                <img src={logo} alt="" />
-                            </Link>
+                                <Link to="/">
+                                    <img src={logo} alt="" />
+                                </Link>
                             </div>
                             <ul className={styles.ul}>
-                                <li> <a href='#'> Home</a> </li>
-                                <li> <a href='#'> Sobre</a> </li>
-                                <li> <a href='#'> Tipo de casa</a> </li>
-                                <li> <a href='#'> Contato</a> </li>
-                                <li> <a href='#'> <button className={styles.btn_gradiente}>Login</button></a> </li>
+                                <li>
+                                    <Link to="/home">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/sobre">Sobre</Link>
+                                </li>
+                                <li>
+                                    <Link to="/tipocasa">Tipo de casa</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contato">Contato</Link>
+                                </li>
+                                <li>
+                                    <Link to="/login"><button className={styles.btn_gradiente}>Login</button></Link>
+                                </li>
                             </ul>
                             <div className={styles.menu_icon}>
                                 <img src={menu} alt='ícone de menu' />
@@ -60,19 +73,19 @@ function LandingPage() {
             <section className={styles.resultados}>
                 <div className={styles.container}>
                     <div className={styles.resultados_text}>
-                        Ajudando pessoas e empresas com foco no conforto.
+                        Ajudando a manter um registro organizado das necessidades e atividades diárias dos idosos sob seus cuidados.
                     </div>
                     <div className={styles.resultados_numero}>
                         <p>
-                            <span>+50 mil</span> <br />
+                            <span>+</span> <br />
                             Cuidados Prestados
                         </p>
                         <p>
-                            <span>+20 mil</span> <br />
+                            <span>+</span> <br />
                             Famílias satisfeitas
                         </p>
                         <p>
-                            <span>+60 mil</span> <br />
+                            <span>+</span> <br />
                             Momentos de carinho
                         </p>
                     </div>
@@ -113,7 +126,8 @@ function LandingPage() {
                     <div className={styles.casa_text}>
                         <h2>Cuidado Compassivo</h2>
                         <p>
-                            Nossa missão é proporcionar cuidados que não apenas atendam às necessidades físicas, mas também nutram a alma. Cada sorriso compartilhado, cada história ouvida é parte do nosso compromisso.
+                            Nossa missão é proporcionar cuidados que não apenas atendam às necessidades físicas, mas também nutram a alma.
+                            Cada sorriso compartilhado, cada história ouvida é parte do nosso compromisso.
                         </p>
                         <img src={user_happy} alt='User Happy' />
                         <p>
@@ -129,37 +143,111 @@ function LandingPage() {
 
                     <div className={styles.cards}>
                         <div className={styles.card_item}>
-                            <img src={casa1} alt='casa1' />
-                            <p className={styles.local_casa}>Medicamentos</p>
+                            <img src={alimentacao} alt='alimentação' />
+
                             <p className={styles.nome_casa}>Alimentação</p>
-                            <p className={styles.contato}>
-                                Rotina 1234 567 8900 <br /> helpsenior@gmail.com
-                                </p>
-                                <button>3 propriedades</button>
+                            <p className={styles.contato}>Registro das preferências alimentares e restrições dietéticas dos idosos, garantindo uma alimentação adequada e saudável.</p>
+
                         </div>
-                        <div className={styles.card_item2}>
-                            <img src={casa2} alt='casa2' />
-                            <p className={styles.local_casa}>Medicamentos</p>
-                            <p className={styles.nome_casa}>Alimentação</p>
-                            <p className={styles.contato}>
-                                Rotina 1234 567 8900 <br /> helpsenior@gmail.com
-                                </p>
-                                <button>3 propriedades</button>
+                        <div className={styles.card_item}>
+                            <img src={medicamentos} alt='medicamentos' />
+
+                            <p className={styles.nome_casa}>Medicamentos</p>
+                            <p className={styles.contato}>Registre detalhes sobre os medicamentos dos idosos e evite erros na administração.</p>
+
                         </div>
-                        <div className={styles.card_item3}>
-                            <img src={casa3} alt='casa3' />
-                            <p className={styles.local_casa}>Medicamentos</p>
-                            <p className={styles.nome_casa}>Alimentação</p>
-                            <p className={styles.contato}>
-                                Rotina 1234 567 8900 <br /> helpsenior@gmail.com
-                                </p>
-                                <button>3 propriedades</button>
+                        <div className={styles.card_item}>
+                            <img src={rotina} alt='rotina' />
+
+                            <p className={styles.nome_casa}>Rotina</p>
+                            <p className={styles.contato}>Crie e gerencie a rotina diária dos idosos de forma flexível e adaptada às suas necessidades.</p>
+
                         </div>
                     </div>
                 </div>
             </section>
+            <section className={styles.newsletter}>
+                <div className={styles.container}>
+                    <div className={styles.box_newsletter}>
+                        <h2>Escreva-se na nossa newletter</h2>
+                        <p>Esgrava-se pra receber conteúdos, novidades e dicas importantes.</p>
+                        <form>
+                            <input type='email' placeholder='Digite o seu email' />
+                            <button>Subescrever</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+
+            <footer>
+                <div className={styles.container}>
+                    <ul>
+                        <h3>HELPSENIOR</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem, ipsum dolor
+                        </p>
+                        <div className={styles.redes_sociais}>
+                            <img src={facebook} alt='facebook' />
+                            <img src={twitter} alt='twitter' />
+                            <img src={instagram} alt='instagram' />
+                            <img src={linkedin} alt='linkedin' />
+                        </div>
+                    </ul>
+
+                    <ul>
+                        <h3>Link</h3>
+                        <li>
+                            <Link to="/home">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/precos">Preços</Link>
+                        </li>
+                        <li>
+                            <Link to="/baixar">Baixar</Link>
+                        </li>
+                        <li>
+                            <Link to="/sobre">Sobre</Link>
+                        </li>
+                        <li>
+                            <Link to="/servicos">Serviços</Link>
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <h3>Suporte</h3>
+                        <li>
+                            <Link to="/home">FAQ</Link>
+                        </li>
+                        <li>
+                            <Link to="/home">Como funciona</Link>
+                        </li>
+                        <li>
+                            <Link to="/home">Caracteristícas</Link>
+                        </li>
+                        <li>
+                            <Link to="/contato">Contato</Link>
+                        </li>
+                        <li>
+                            <Link to="/comunicando">Comunicando</Link>
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <h3>Nos contate</h3>
+                        <li>
+                            <p>+000 12345678</p>
+                        </li>
+                        <li>
+                            <p>helpsenior@gmail.com</p>
+                        </li>
+                        <li>
+                            <p>Brasil</p>
+                        </li>
+
+                    </ul>
+                </div>
+            </footer>
         </div>
     );
 }
 
-export default LandingPage;
+export default LandingPage
